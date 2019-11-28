a53d265099f4613e5f270de9b63ec66b5c360469
Portia-arch <portiangobese49@gmail.com>
2019-11-28 16:58:12 +0200

    It's getting there

--- game.js
+++ game.js
@@ -1,8 +1,43 @@
 //Card Data
 
-const cardArray = [{
+//Card array
+const game = document.getElementById("game")
+const grid = document.createElement("section");
+grid.setAttribute('class', 'grid');
 
-    //for 
+game.appendChild(grid)
 
-    
-}]
+const cards = document.querySelector(".card");
+
+// Randomize game grid on each load
+grid.sort(() => 0.5 - Math.random())
+
+//Export cards
+let unlockedCard = false;
+let lockedBoard = false;
+let card1, card2;
+
+//shuffles cards every time the game starts or restarts
+(function startGame() {
+    shuffle();
+});
+
+// Event listener to grid
+grid.addEventListener('click', function(event) {
+    // The event target is our clicked item
+    let clicked = event.target
+  
+    // Do not allow the grid section itself to be selected; only select divs inside the grid
+    if (clicked.nodeName === 'SECTION') {
+      return
+    }
+  
+    // Add selected class
+    clicked.classList.add('clicked')
+  })
+
+
+
+// const cardArray = [{
+//     game.Grid
+// }]
