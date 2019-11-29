bce88af050bc6c4c2614acd7a575559e6f764aef
Portia-arch <portiangobese49@gmail.com>
2019-11-29 14:35:40 +0200

    javascript changes

--- game.js
+++ game.js
@@ -1,11 +1,56 @@
 //Card Data
-const cards = document.querySelector(".card")
+const cards = document.querySelectorAll('.card');
 
-function flipcard(){
-this.classList.toggle(flipcard)
+ let flippedTrig = false;
+ //the flipping of cards must be disabled if two sets of cards have been opened
+ let lockBoard = false;
+ let cardOne, cardTwo;
 
-  console.log("You just clicked me!");
-  console.log(this);
+function flipTrigger() {
+  this.classList.add('flip');
+ 
+  //the first flip
+  if (!flipTrigger) {
+    flipTrigger = true;
+    cardOne = this;
+
+  } else
+    {
+      // the second flip
+      flipTrigger = false;
+      cardTwo = this;
+  }
+
+  //check if the flipped cards match
+    console.log(cardOne.dataset.name);
+    console.log(cardTwo.dataset.name);
+
+  // function cardmatch() {
+  //   if (cardOne.dataset.name === cardTwo.data-name){
+  //     return
+  //   }
+  // }
+
 }
 
-cards.forEach(card => card.addEventListner("click", flipcard));
+cards.forEach(card => card.addEventListener('click', flipTrigger));
+
+
+
+// function flippedCard() {
+  
+
+// };
+
+
+
+
+// grid.addEventListener("click", function() {
+//   let selected = event.target
+
+//   if (selected.nodeName === "section") {
+//     return
+//   }
+
+//   selected.classList.add("selected")
+// })
